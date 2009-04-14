e9dc5a136461a77bbeb579f797e2c8e0df5a3a54
guillaume.pilot <guillaume.pilot@520ee298-0d87-11de-bc6d-132d5dcf21d4>
2009-04-14 19:36:33 +0000

    Envoi mail OK

--- trunk/phpbbextract/src/sendMailServlet.java
+++ trunk/phpbbextract/src/sendMailServlet.java
@@ -1,6 +1,8 @@
 
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -37,93 +39,70 @@ public class sendMailServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//sendMail("marine.canessa@gmail.com","coucou","D:/eclipse/save.xml");
-		System.out.println("coucou");
-		envoyerMailSMTP("localhost", true);
+		
+        String from = request.getParameter("from");
+    	String to = request.getParameter("to");
+    	String subject = request.getParameter("subject");
+    	String message = request.getParameter("message");
+    	String login = request.getParameter("login");
+    	String password = request.getParameter("password");
+    	System.out.println(from+ to+subject+message+login+password);
+		envoyerMailSMTP(from, to,subject,message,login,password);
+		//processRequest(request, response);
+
 	}
 	
-	public static boolean envoyerMailSMTP(String serveur, boolean debug) {   
-        boolean result = false;   
-        try {   
-                 Properties prop = System.getProperties();   
-                 prop.put("mail.smtp.host", serveur);   
-                 Session session = Session.getDefaultInstance(prop,null);   
-                 Message message = new MimeMessage(session);   
-                 message.setFrom(new InternetAddress("marine.canessa@gmail.com"));   
-                 InternetAddress[] internetAddresses = new InternetAddress[1];   
-                 internetAddresses[0] = new InternetAddress("marine.canessa@gmail.com");   
-                 message.setRecipients(Message.RecipientType.TO,internetAddresses);   
-                 message.setSubject("Test");   
-                 message.setText("test mail");   
-                 message.setHeader("X-Mailer", "Java");   
-                 message.setSentDate(new Date());   
-                 session.setDebug(debug);   
-                 Transport.send(message);   
-                 result = true;   
-        } catch (AddressException e) {   
-                 e.printStackTrace();   
-        } catch (MessagingException e) {   
-                 e.printStackTrace();   
-        }   
-        return result;   
-  }   
+ 	public static void envoyerMailSMTP(String from,String to,String subject,String message,String login,String password) {   
+        	try {
+        		Properties props = new Properties();
+        		props.setProperty("mail.host", "smtp.gmail.com");
+        		props.setProperty("mail.smtp.port", "587");
+        		props.setProperty("mail.smtp.auth", "true");
+        		props.setProperty("mail.smtp.starttls.enable", "true");
+        		String filename = "d:/eclipse/save.xml";
+        		Authenticator auth = new SMTPAuthenticator(login, password);
 
-		/*public static void sendMail( String to, String subject, String file )
-		{
-			try
-			{
-				// Create some properties and get the default Session;
-				Properties props = System.getProperties();
-				props.put("mail.smtp.host", "localhost.server");
-				Session session = Session.getDefaultInstance(props, null);
-				String from = "marine.canessa@gmail.com";
-				
-//				Crée le message
-				Message message = new MimeMessage(session);
-				
-//				On met les attributs d'entête ( sujet, adresse, expéditeur, destinataire)
-				message.setSubject("Embedded Image");
-				message.setFrom(new InternetAddress(from));
-				message.addRecipient(Message.RecipientType.TO,
-						new InternetAddress(to));
-				
-//				Crée la partie message pour le contenu
-				BodyPart messageBodyPart = new MimeBodyPart();
-				String htmlText = "<H1>Hello</H1>" +
-				"<img src=\"cid:image\">";
-				
-//				Type et sous-type du message
-				messageBodyPart.setContent( htmlText, "text/html" );
-				
-//				Crée l'objet Multipart qui contiendra toutes les parties du message
-//				on doit passer en paramètre "related", puisque les deux parties entres elles
-//				sont reliées
-				MimeMultipart multipart = new MimeMultipart( "related" );
-				multipart.addBodyPart( messageBodyPart );
-				
-//				Crée l'autre partie du message qui contient l'image
-				messageBodyPart = new MimeBodyPart();
-				
-//				Place l'image dans la partie
-				DataSource fds = new FileDataSource( file );
-				messageBodyPart.setDataHandler( new DataHandler(fds) );
-				
-//				Attribue un nom à l'entête de l'image ( pour faire le lien)
-				messageBodyPart.setHeader("Content-ID", "<image>");
-				
-//				Ajoute la partie à l'objet Multipart
-				multipart.addBodyPart( messageBodyPart  );
-				
-//				Ajoute l'objet Multipart au message
-				message.setContent(multipart);
-				
-				Transport.send(message);
-			}
-			catch (Exception ex) 
-			{
-				ex.printStackTrace();
-			}
-		}*/
+        		Session session = Session.getInstance(props, auth);
+
+        		MimeMessage msg = new MimeMessage(session);
+        		
+        		// Create the message part
+        		Multipart mp = new MimeMultipart(); 
+    			
+        		
+        		msg.setText(message);
+        		msg.setSubject(subject);
+        		msg.setFrom(new InternetAddress(from));
+        		msg.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+        		msg.setContent(mp);
+        		MimeBodyPart mbp2 = new MimeBodyPart();			
+        		DataSource ds = new FileDataSource(filename);
+	     		mbp2.setDataHandler(new DataHandler(ds));		   
+	     		mbp2.setFileName("save.xml");			   
+		     	mp.addBodyPart(mbp2);
+        		Transport.send(msg);
 
 
+        	} catch (AuthenticationFailedException ex) {
+        		ex.printStackTrace();
+        	} catch (AddressException ex) {
+        		ex.printStackTrace();
+        	} catch (MessagingException ex) {
+        		ex.printStackTrace();
+        	}
+ 	}
+        
+       
+        private static class SMTPAuthenticator extends Authenticator {
+
+            private PasswordAuthentication authentication;
+
+            public SMTPAuthenticator(String login, String password) {
+                authentication = new PasswordAuthentication(login, password);
+            }
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return authentication;
+            }
+        }
 }
